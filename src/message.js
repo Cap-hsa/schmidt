7c343bbeeb8786c3419bebd416d38f2110574026
Hugo Saracino <hugo.saracino@gmail.com>
2017-05-19 16:42:19 +0200

    Small fix bis

--- src/message.js
+++ src/message.js
@@ -19,64 +19,71 @@ const replyMessage = (message) => {
 
   // Call Recast.AI SDK, through /converse route
   request.converseText(text, { conversationToken: senderId })
-  .then(result => {
-    /*
-    * YOUR OWN CODE
-    * Here, you can add your own process.
-    * Ex: You can call any external API
-    * Or: Update your mongo DB
-    * etc...
-    */
-    if (result.action) {
-      console.log('The conversation action is: ', result.action.slug)
-    }
+    .then(result => {
+      /*
+      * YOUR OWN CODE
+      * Here, you can add your own process.
+      * Ex: You can call any external API
+      * Or: Update your mongo DB
+      * etc...
+      */
+      if (result.action) {
+        console.log('The conversation action is: ', result.action.slug)
+      }
 
-    const rep = (content, action) => {
-      if (action) {
-        if (action.slug === 'catalogue_cuisine') {
-          return [
-            {
-              type: 'text',
-              content,
-            },
-            {
-              type: 'card',
-              content: {
-                title: 'Catalogue',
-                subtitle: 'Cuisines',
-                imageUrl: '',
+      const rep = (content, action) => {
+        if (action) {
+          if (action.slug === 'catalogue_cuisine') {
+            return [
+              {
+                type: 'text',
+                content,
               },
-            },
-          ]
-        }
+              {
+                type: 'card',
+                content: {
+                  title: 'Catalogue',
+                  subtitle: 'Cuisines',
+                  imageUrl: '',
+                  buttons: [
+                    {
+                      title: 'Download',
+                      type: 'web_url',
+                      value: 'http://www.cuisines-schmidt.com/catalogues_pdf/cuisine/CUISINE_FR.pdf',
+                    },
+                  ],
+                },
+              },
+            ]
+          }
 
-        return {
-          type: 'text',
-          content,
+          return {
+            type: 'text',
+            content,
+          }
         }
       }
-    }
 
-    // If there is not any message return by Recast.AI for this current conversation
-    if (!result.replies.length) {
-      message.addReply({ type: 'text', content: 'I don\'t have the reply to this yet :)' })
-    } else {
-      // Add each reply received from API to replies stack
-      result.replies.forEach(replyContent => message.addReply(rep(replyContent, result.action)))
-    }
+      // If there is not any message return by Recast.AI for this current conversation
+      if (!result.replies.length) {
+        message.addReply({ type: 'text', content: 'I don\'t have the reply to this yet :)' })
+      } else {
+        // Add each reply received from API to replies stack
+        result.replies.forEach(replyContent => message.addReply(rep(replyContent, result.action)))
+      }
 
-    // Send all replies
-    message.reply()
-    .then(() => {
-      // Do some code after sending messages
+      // Send all replies
+      message.reply()
+        .then(() => {
+          // Do some code after sending messages
+        })
+        .catch(err => {
+          console.error('Error while sending message to channel', err)
+        })
     })
     .catch(err => {
-      console.error('Error while sending message to channel', err)
+      console.error('Error while sending message to Recast.AI', err)
     })
-  })
-  .catch(err => {
-    console.error('Error while sending message to Recast.AI', err)
-  })
 }
 
 module.exports = replyMessage
